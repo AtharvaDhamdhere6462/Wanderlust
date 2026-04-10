32f7527de5ad719db8bc999f4221ebba87f57d75
Atharva Dhamdhere <atharva.dhamdhere6462@gmail.com>
2026-04-10 10:51:32 +0530

    fixed error

--- app.js
+++ app.js
@@ -120,6 +120,8 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(8080, () => {
-    console.log("sever is listen to port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`);
 });
