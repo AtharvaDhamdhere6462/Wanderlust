215ebdaefa9a6d8776c9ed2e274ddcec2b50c8a1
Atharva Dhamdhere <atharva.dhamdhere6462@gmail.com>
2026-04-10 11:13:46 +0530

    fixed error

--- app.js
+++ app.js
@@ -98,6 +98,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 // app.get("/fakeUser", async(req, res) => {
 //     const user = new User({
 //         email: "fake@example.com",
